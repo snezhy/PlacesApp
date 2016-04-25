90e2063811b9a1c892dadad1e5366473677bf393
snezhy <Snezhana@Snezhanas-MacBook-Pro.local>
2016-04-25 23:10:24 +0100

    adding controller and view for places, creating app.js

--- app/app.js
+++ app/app.js
@@ -1,3 +1,14 @@
 /**
  * Created by Snezhana on 25/04/16.
  */
+
+var app = angular.module('PlacesApp', ['ngRoute', 'ngResource', 'ui.bootstrap']);
+app.config(function ($routeProvider) {
+
+    $routeProvider.when("/explore", {
+        controller: "placesController",
+        templateUrl: "/app/views/places.html"
+    });
+    $routeProvider.otherwise({ redirectTo: "/explore" });
+
+});
---
+++ app/controllers/placesController.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Snezhana on 25/04/16.
+ */
+
+app.controller('placesController', function ($scope) {
+    $scope.exploreNearby = "London";
+});
