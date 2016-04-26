380a656ab2018f06b2a9614f28e573adfe8262ca
snezhy <Snezhana@Snezhanas-MacBook-Pro.local>
2016-04-26 07:53:49 +0100

    load placeFilter.js

--- app/controllers/placesController.js
+++ app/controllers/placesController.js
@@ -23,7 +23,7 @@ app.controller('placesController', function ($scope, placesService, $filter) {
 
     function init() {
 
-       // createWatch();
+        createWatch();
         getPlaces();
     }
 
@@ -32,11 +32,11 @@ app.controller('placesController', function ($scope, placesService, $filter) {
         var offset = ($scope.pageSize) * ($scope.currentPage - 1);
 
         placesService.get({ near: $scope.exploreNearby, query: $scope.exploreQuery, limit: $scope.pageSize, offset: offset }, function (placesResult) {
-    
-            if (placesResult.response.groups) {
+
+            if (placesResult.response.groups) {console.log("here");
                 $scope.places = placesResult.response.groups[0].items;
                 $scope.totalRecordsCount = placesResult.response.totalResults;
-                //filterPlaces('');
+                filterPlaces('');
             }
             else {
                 $scope.places = [];
@@ -45,37 +45,37 @@ app.controller('placesController', function ($scope, placesService, $filter) {
         });
     };
 
-//    function filterPlaces(filterInput) {
-//        $scope.filteredPlaces = $filter("placeFilter")($scope.places, filterInput);
-//        $scope.filteredPlacesCount = $scope.filteredPlaces.length;
-//    }
-//
-//    function createWatch() {
-//
-//        $scope.$watch("filterValue", function (filterInput) {
-//            filterPlaces(filterInput);
-//        });
-//    }
-//
-//    $scope.doSearch = function () {
-//
-//        $scope.currentPage = 1;
-//        getPlaces();
-//    };
-//
-//    $scope.pageChanged = function (page) {
-//
-//        $scope.currentPage = page;
-//        getPlaces();
-//    };
-//
-//    $scope.buildCategoryIcon = function (icon) {
-//
-//        return icon.prefix + '44' + icon.suffix;
-//    };
-//
-//    $scope.buildVenueThumbnail = function (photo) {
-//
-//        return photo.items[0].prefix + '128x128' + photo.items[0].suffix;
-//    };
+    function filterPlaces(filterInput) {
+        $scope.filteredPlaces = $filter("placeFilter")($scope.places, filterInput);
+        $scope.filteredPlacesCount = $scope.filteredPlaces.length;
+    }
+
+    function createWatch() {
+
+        $scope.$watch("filterValue", function (filterInput) {
+            filterPlaces(filterInput);
+        });
+    }
+
+    $scope.doSearch = function () {
+
+        $scope.currentPage = 1;
+        getPlaces();
+    };
+
+    $scope.pageChanged = function (page) {
+
+        $scope.currentPage = page;
+        getPlaces();
+    };
+
+    $scope.buildCategoryIcon = function (icon) {
+
+        return icon.prefix + '44' + icon.suffix;
+    };
+
+    $scope.buildVenueThumbnail = function (photo) {
+
+        return photo.items[0].prefix + '128x128' + photo.items[0].suffix;
+    };
 });
